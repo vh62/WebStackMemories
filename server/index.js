68ac0935df8cdbd07a921724286fa93e5f79a622
Vikram <vikram.hwm@gmail.com>
2021-02-14 04:03:18 -0500

    committing to heroku

--- server/index.js
+++ server/index.js
@@ -2,20 +2,24 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js'
 
 const app = express();
+dotenv.config()
 
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-const CONNECTIONURL = 'mongodb+srv://Vikram86:Edward81@cluster0.v2ans.mongodb.net/WebStack?retryWrites=true&w=majority'
+
+app.get('/', (req, res)=> {
+    res.send('Hello to memories API')
+})
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTIONURL, {useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTIONURL, {useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=> app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error )=> console.log(error.message))
 
